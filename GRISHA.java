80844f53777c1304d7f3c2ea47c55212268fb4e2
MaximDubovik <39271474+MaximDubovik@users.noreply.github.com>
2018-05-16 01:12:27 +0300

    Add files via upload

---
+++ GRISHA.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class GRISHA {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 32, 545, 767, 9789};
+        for (int i = 0; i < numbers.length; i++) {
+
+                System.out.println(numbers[i]);
+            }
+
+            for (int i = 0; i < numbers.length; i++) {
+                System.out.print(numbers[i]);
+            }
+        }
+}
